f420601d44198cc49323698e35700815b1f9d0f0
luiscarlosjp <jardimpereiralc@gmail.com>
2025-08-27 10:55:39 -0300

    feat: add test to errors in Calculator1

--- src/calculators/calculator_1_test.py
+++ src/calculators/calculator_1_test.py
@@ -1,5 +1,6 @@
 from .calculator_1 import Calculator1
 from typing import Dict
+from pytest import raises
 
 class MockRequest:
     def __init__(self, body: Dict) -> None:
@@ -17,3 +18,12 @@ def test_Calculate():
 
     assert response["data"]["result"] == 14.25
     assert response["data"]["Calculator"] == 1
+
+def test_calculate_with_body_error():
+    mock_request =  MockRequest(body={ "something": 1})
+    calculator_1 = Calculator1()
+    
+    with raises(Exception) as excinfo:
+        calculator_1.calculate(mock_request)
+    
+    assert str(excinfo.value) == 'body mal formatado!'
